c43dcc35a1782a3de911e761e7d971292a003ecb
Mauro-666 <eltercodesiempre73@gmail.com>
2024-05-16 00:12:19 -0300

    16-5

--- frontend/src/pages/Inicio.js
+++ frontend/src/pages/Inicio.js
@@ -12,6 +12,40 @@ const InicioPage = (props) => {
             </div>
             <div className="contenido">
                 <h1>Agendemos tu turno</h1>
+                <div className="cards">
+                    <div className="card1">
+                        <div className="texto-card">
+                            <h3>Reiki</h3>
+                            <p>kkjf lkdlso allsj fjjla la ljsljf la jsl ajsjidifhg al</p>
+                        </div>
+                    </div>
+                    <div className="card2">
+                        <div className="texto-card">
+                            <h3>Reiki</h3>
+                            <p>kkjf lkdlso allsj fjjla la ljsljf la jsl ajsjidifhg al</p>
+                        </div>
+                    </div>
+                    <div className="card3">
+                        <div className="texto-card">
+                            <h3>Reiki</h3>
+                            <p>kkjf lkdlso allsj fjjla la ljsljf la jsl ajsjidifhg al</p>
+                        </div>
+                    </div>
+                    <div className="card4">
+                        <div className="texto-card">
+                            <h3>Reiki</h3>
+                            <p>kkjf lkdlso allsj fjjla la ljsljf la jsl ajsjidifhg al</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="contenido2">
+                <div className="card5"></div>
+                <div className="texto-card">
+                    <h1> MIERCOLES </h1>
+                    <h3> Meditacion y yoga </h3>
+                    <p> jgjfkñ n jkajdñ asjka ñsldjf añsdjf añsjd falksjd fñaksjdñf asldk fjasdfhas dgña </p>
+                </div>
             </div>
         </div>
 
